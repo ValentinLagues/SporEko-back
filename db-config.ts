2cdf762c99f82c59757c28c97471e412f7eb07c9
Valentin LAGUES <86538134+ValentinLagues@users.noreply.github.com>
2022-07-17 16:25:41 +0200

    Update db-config.ts

--- db-config.ts
+++ db-config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import mysql, { Connection } from 'mysql2';
 
 const connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST, // address of the server
-  port: Number(process.env.DB_PORT), // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: ec2-54-76-43-89.eu-west-1.compute.amazonaws.com,
+  port: 5432,
+  user: iivnuzihfazuxo,
+  password: 58978e89fd5c2a57846257d97951d54ef014900e5f0b4bc7e77310aa90b80199,
+  database: dchgkdc5pmqfub,
 });
 
 export default connection;
